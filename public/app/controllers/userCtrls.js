73993992f529c832ff6a9de7145b484f3766c46c
Perry Craft <LjCraft12@gmail.com>
2017-01-21 12:08:11 -0500

    working on facebook login

--- public/app/controllers/userCtrls.js
+++ public/app/controllers/userCtrls.js
@@ -16,9 +16,13 @@ angular.module('userController', ['userServices'])
                     }, 1500)
                 } else {
                     //create error message
-                    app.loading = false
+                    app.loading = false;
                     app.errorMessage = data.data.message;
                 }
             })
         }
-    });
+    })
+
+.controller('facebookCtrl', function ($routeParams) {
+    console.log($routeParams.token);
+});
