5dfb90bb3b12a61589a47e87167dc3eeefcfb537
Makai Takori <makait87@icloud.com>
2017-03-14 19:44:49 -0400

    created and coded JS to make weather app functional

--- public/assets/js/weather.js
+++ public/assets/js/weather.js
@@ -1,3 +1,37 @@
+if("geolocation" in navigator){
+    navigator.geolocation.getCurrentPosition(function (position) {
+       loadWeather(position.coords.latitude + "," + position.coords.longitude);                                         //*checks that users browser supports geolocation API
+    });
+}   else {
+    loadWeather("New York, NY", "");
+}
 
+$(document).ready(function () {
+    setInterval(getWeather, 100000);                                                                                     //*makes sure browser refreshes weather quickly
 
-alert("hello world");
+});
+
+function loadWeather(location, woeid) {
+    $.simpleWeather({
+        location: location,
+        woeid: woeid,
+        unit:'f',
+        success:function (weather) {
+            city = weather.city;
+            temp = weather.temp+'&deg;';
+            wcode = '<img class="weathericons" src= "assets/images/Weather/weathericons/' + weather.code + '.svg">';
+            wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
+            humidity = weather.humidity + ' %';
+
+            $(".location").text(city);
+            $(".temperature").html(temp);
+            $(".climate_bg").html(wcode);
+            $(".windspeed").html(wind);
+            $(".humidity").text(humidity);
+        },
+        
+        error: function (error) {
+            $(".error").html('<p>' + error + '</p>');
+        }
+    });
+}
